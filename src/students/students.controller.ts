ee0d93e019eb1372ca8f6ae12822f3c3b25dfc35
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-08-16 20:39:52 +0600

    Insert Data into MongoDB using Mongoose

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -9,44 +9,14 @@ import {
   Put,
 } from '@nestjs/common';
 import { StudentsService } from './students.service';
+import { Student } from './student.schema';
 
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
-  @Get()
-  getAllStudents() {
-    return this.studentsService.getAllStudents();
-  }
-
-  @Get(':id')
-  getStudentsById(@Param('id') id: string) {
-    return this.studentsService.getStudentsById(Number(id));
-  }
-
   @Post()
-  createStudent(@Body() body: { name: string; age: number }) {
-    return this.studentsService.createStudent(body);
-  }
-
-  @Put(':id')
-  updateStudent(
-    @Param('id') id: string,
-    @Body() body: { name: string; age: number },
-  ) {
-    return this.studentsService.updateStudent(Number(id), body);
-  }
-
-  @Patch(':id')
-  patchStudent(
-    @Param('id') id: string,
-    @Body() body: Partial<{ name: string; age: number }>,
-  ) {
-    return this.studentsService.patchStudent(Number(id), body);
-  }
-
-  @Delete(':id')
-  deleteStudent(@Param('id') id: string) {
-    return this.studentsService.deleteStudent(Number(id));
+  async addStudent(@Body() data: Partial<Student>) {
+    return this.studentsService.createStudent(data);
   }
 }
--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -7,6 +7,9 @@ import { Student, StudentSchema } from './student.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema }]),
+    //class name is student.name not every students name
+    // student.name	একজন student's name
+    // Student.name	class-এর নাম "Student"
   ],
   controllers: [StudentsController],
   providers: [StudentsService],
